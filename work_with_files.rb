ad24310e64d4d10d7431f36a7f927470664aeb3a
Sofia Bendus <54141095+SofiiaPro@users.noreply.github.com>
2021-05-14 10:55:31 +0300

    git commit -m "refactor code with rubocop"

--- work_with_files.rb
+++ work_with_files.rb
@@ -7,43 +7,41 @@ my_file = File.open(file_name, "w") do |f|
   f.write(file_text)
 end
 
-#read text
+# read text
 puts "Do you want to read text from your file? Y/N"
 read_file_answer = gets.chomp.upcase!
 if read_file_answer == "Y"
 
-puts "The content of your file:"
-my_file = File.open(file_name, "r") {
-  |f| f.each_line { |line| puts line }
-}
+  puts "The content of your file:"
+  my_file = File.open(file_name, "r") { |f|
+    f.each_line { |line| puts line }
+  }
 elsif read_file_answer == "N"
   puts "Thank you for the answer!"
 else
   puts "Error! Your answer should be Y/N"
 end
 
-#close file
+# close file
 puts "Do you want to close your file? Y/N"
 close_file_answer = gets.chomp.upcase!
 if close_file_answer == "Y"
-my_file.close
-if my_file.closed?
-  puts "File #{file_name} was closed successfully"
-else
-  puts "File #{file_name} hasn't been closed!"
-end
+  my_file.close
+  if my_file.closed?
+    puts "File #{file_name} was closed successfully"
+  else
+    puts "File #{file_name} hasn't been closed!"
+  end
 elsif close_file_answer == "N"
   puts "Thank you for the answer!"
-else
-  puts "Error! Your answer should be Y/N"
 end
 
-#delete file
+# delete file
 puts "Do you want to delete your file? Y/N"
 delete_file_answer = gets.chomp.upcase!
 if delete_file_answer == "Y"
   File.delete(my_file)
-  if !File.exists?(file_name)
+  if !File.exist?(file_name)
     puts "File #{file_name} was deleted successfully"
   else
     puts "File #{file_name} hasn't been deleted!"
